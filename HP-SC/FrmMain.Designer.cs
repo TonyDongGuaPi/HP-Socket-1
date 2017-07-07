7607cdf25e51388ac050e77f38312528041a2191
zzjincnCompany <zzjincnCompany@DESKTOP-Q3MNUK9>
2017-07-07 09:02:43 +0800

    start

---
+++ HP-SC/FrmMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace HP_SC
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDisconn = new System.Windows.Forms.Button();
+            this.txtDisConn = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbxMsg = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "连接ID：";
+            // 
+            // btnDisconn
+            // 
+            this.btnDisconn.Location = new System.Drawing.Point(300, 13);
+            this.btnDisconn.Name = "btnDisconn";
+            this.btnDisconn.Size = new System.Drawing.Size(47, 26);
+            this.btnDisconn.TabIndex = 21;
+            this.btnDisconn.Text = "断开";
+            this.btnDisconn.UseVisualStyleBackColor = true;
+            this.btnDisconn.Click += new System.EventHandler(this.btnDisconn_Click);
+            // 
+            // txtDisConn
+            // 
+            this.txtDisConn.Location = new System.Drawing.Point(255, 16);
+            this.txtDisConn.Name = "txtDisConn";
+            this.txtDisConn.Size = new System.Drawing.Size(40, 21);
+            this.txtDisConn.TabIndex = 20;
+            this.txtDisConn.Text = "0";
+            this.txtDisConn.TextChanged += new System.EventHandler(this.txtDisConn_TextChanged);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(154, 16);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(40, 21);
+            this.txtPort.TabIndex = 19;
+            this.txtPort.Text = "5555";
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(56, 16);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(99, 21);
+            this.txtIpAddress.TabIndex = 18;
+            this.txtIpAddress.Text = "192.168.0.122";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Location = new System.Drawing.Point(427, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(47, 26);
+            this.btnStop.TabIndex = 17;
+            this.btnStop.Text = "关闭";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(374, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(47, 26);
+            this.btnStart.TabIndex = 16;
+            this.btnStart.Text = "开启";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "IP：";
+            // 
+            // lbxMsg
+            // 
+            this.lbxMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbxMsg.FormattingEnabled = true;
+            this.lbxMsg.ItemHeight = 12;
+            this.lbxMsg.Location = new System.Drawing.Point(12, 45);
+            this.lbxMsg.Name = "lbxMsg";
+            this.lbxMsg.Size = new System.Drawing.Size(488, 316);
+            this.lbxMsg.TabIndex = 23;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 373);
+            this.Controls.Add(this.lbxMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDisconn);
+            this.Controls.Add(this.txtDisConn);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIpAddress);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "主界面";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDisconn;
+        private System.Windows.Forms.TextBox txtDisConn;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbxMsg;
+    }
+}
+
---
+++ HP-SCClient/FrmMain.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace HP_SCClient
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendSerializableObject = new System.Windows.Forms.Button();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxAsyncConn = new System.Windows.Forms.CheckBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbxMsg = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSendSerializableObject
+            // 
+            this.btnSendSerializableObject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSendSerializableObject.Location = new System.Drawing.Point(408, 387);
+            this.btnSendSerializableObject.Name = "btnSendSerializableObject";
+            this.btnSendSerializableObject.Size = new System.Drawing.Size(163, 26);
+            this.btnSendSerializableObject.TabIndex = 21;
+            this.btnSendSerializableObject.Text = "Send serializable object";
+            this.btnSendSerializableObject.UseVisualStyleBackColor = true;
+            this.btnSendSerializableObject.Click += new System.EventHandler(this.btnSendSerializableObject_Click);
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSendFile.Location = new System.Drawing.Point(327, 387);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(75, 26);
+            this.btnSendFile.TabIndex = 20;
+            this.btnSendFile.Text = "发送文件";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(514, 361);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(47, 20);
+            this.btnSend.TabIndex = 19;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSend.Location = new System.Drawing.Point(12, 360);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(473, 21);
+            this.txtSend.TabIndex = 18;
+            this.txtSend.Text = "text to be sent";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbxAsyncConn);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.txtIpAddress);
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(559, 47);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "IP：";
+            // 
+            // cbxAsyncConn
+            // 
+            this.cbxAsyncConn.AutoSize = true;
+            this.cbxAsyncConn.Checked = true;
+            this.cbxAsyncConn.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbxAsyncConn.Location = new System.Drawing.Point(185, 20);
+            this.cbxAsyncConn.Name = "cbxAsyncConn";
+            this.cbxAsyncConn.Size = new System.Drawing.Size(72, 16);
+            this.cbxAsyncConn.TabIndex = 13;
+            this.cbxAsyncConn.Text = "异步连接";
+            this.cbxAsyncConn.UseVisualStyleBackColor = true;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(132, 18);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(40, 21);
+            this.txtPort.TabIndex = 12;
+            this.txtPort.Text = "5555";
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(46, 18);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(87, 21);
+            this.txtIpAddress.TabIndex = 11;
+            this.txtIpAddress.Text = "192.168.0.122";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Location = new System.Drawing.Point(502, 15);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(47, 26);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(449, 15);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(47, 26);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "开启";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbxMsg
+            // 
+            this.lbxMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbxMsg.FormattingEnabled = true;
+            this.lbxMsg.ItemHeight = 12;
+            this.lbxMsg.Location = new System.Drawing.Point(12, 65);
+            this.lbxMsg.Name = "lbxMsg";
+            this.lbxMsg.Size = new System.Drawing.Size(559, 280);
+            this.lbxMsg.TabIndex = 16;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 426);
+            this.Controls.Add(this.btnSendSerializableObject);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbxMsg);
+            this.Name = "FrmMain";
+            this.Text = "主页面";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMain_FormClosed);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendSerializableObject;
+        private System.Windows.Forms.Button btnSendFile;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cbxAsyncConn;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lbxMsg;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
